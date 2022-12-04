20eefb5b3e44f8e1502d17917babc7970629ef16
Daiane Mascarenhas <adm.daianesilva@gmail.com>
2022-12-04 19:28:21 -0300

    feature: eslint and prettier config

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,8 +4,6 @@ import { StatusBar } from 'expo-status-bar';
 import { styles } from './styles';
 
 export const Home = () => {
-  const a = 12;
-
   return (
     <View style={styles.container}>
       <Text>Open up App.tsxmojko to start working on your app!</Text>
