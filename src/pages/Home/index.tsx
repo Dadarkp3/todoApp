b06ee4d64b34a8135d335b8a26047d3b60f0ad6d
Daiane Mascarenhas <adm.daianesilva@gmail.com>
2022-12-04 19:24:52 -0300

    feature: eslint and prettier config

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 import { styles } from './styles';
 
 export const Home = () => {
+  const a = 12;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsxmojko to start working on your app!</Text>
-      <StatusBar style='auto' />
+      <StatusBar style="auto" />
     </View>
   );
 };
